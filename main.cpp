1f7e7c2b99f1962be8dca8cd1245d65f9e3bf4d1
Kunal Gupta <kunal.gupta@myself.com>
2017-11-19 06:15:08 +0530

    Nearly Done.

--- main.cpp
+++ main.cpp
@@ -16,18 +16,20 @@ bool contains(const std::vector<T> &vec, const T &value) {
     return std::find(vec.begin(), vec.end(), value) != vec.end();
 }
 
-vector<int> keys;
+vector<unsigned int> keys;
 
 void generateData() {
-    vector<int> keys;
-    int num;
-    int maxValue = INT_MAX;
-    fstream input_data("/home/kunal/Documents/GITPRO/Parallel-Hashing-Implementation/test/input1", ios::out);
+    vector<unsigned int> keys;
+    unsigned int num;
+    unsigned int maxValue = INT_MAX;
+    fstream input_data("/home/kunal/Documents/GITPRO/Parallel-Hashing-Implementation/test/input2", ios::out);
     if (!input_data) {
         cout << "IO Error";
         return;
     }
-    for (int i = 0; i < N; ++i) {
+    for (unsigned int i = 0; i < N; ++i) {
+        if (i % 10'000 == 0)
+            cerr << i << endl;
         num = rand() % (maxValue);
         while (contains(keys, num))
             num = rand() % (maxValue);
@@ -46,61 +48,66 @@ void getData(string filename) {
         cout << "IO Error";
         return;
     }
-    int num;
-    for (int i = 0; i < N; ++i) {
+    unsigned int num;
+    for (unsigned int i = 0; i < N; ++i) {
         input_data >> num;
-//        cout<<num<<' ';
         keys.push_back(num);
     }
     input_data.close();
 }
 
-int getBucketNumber(int key, int noOfBuckets) {
-    return key % noOfBuckets;
+unsigned int getBucketNumber(unsigned int key, unsigned int noOfBuckets) {
+    unsigned int c1 = 5;
+    unsigned int c0 = 7;
+    unsigned int prime = 1900813;
+
+    c1 = 1;
+    c0 = 0;
+    prime = key + 1;
+    return (((c0 + c1 * key) % prime) % noOfBuckets);       // Else returning negative values.
 }
 
 class HashFunctions {
 public:
 
-    int c0, c1;
+    unsigned int c0, c1;
 
     HashFunctions() {
         this->c0 = this->c1 = 1;
     }
 
-    HashFunctions(int c0, int c1) {
+    HashFunctions(unsigned int c0, unsigned int c1) {
         this->c0 = c0;
         this->c1 = c1;
     }
 
-    int g(int key) {
-        return abs(((c0 + c1 * key) % 1900813) % TABLE_SIZE);
+    unsigned int g(unsigned int key) {
+        return (((c0 + c1 * key) % 1900813) % TABLE_SIZE);       // Else returning negative values.
     }
 };
 
 int main() {
-//    generateData();
     // Gets data from the input file.
-    getData("/home/kunal/Documents/GITPRO/Parallel-Hashing-Implementation/test/input1");
-////    Debugging purposes
-//    for (auto x: keys) {
-//        cout << x << ' ';
-//    }
-    int noOfBuckets = ceil(N / 409);
+    getData("/home/kunal/Documents/GITPRO/Parallel-Hashing-Implementation/test/input2"); // Contains 1'00'00'000 unique integers
 
-    vector<vector<vector<int> > > hashTable;
+    unsigned int noOfBuckets = ceil(N / 409);
+
+    vector<vector<vector<unsigned int> > > hashTable;
     hashTable.resize(noOfBuckets);
     // Sets up the Hash Tables
-    for (int i = 0; i < noOfBuckets; ++i) {
+#pragma omp parallel for
+    for (unsigned int i = 0; i < noOfBuckets; ++i) {
         hashTable[i].resize(3);
-        for (int j = 0; j < 3; ++j) {
+#pragma omp parallel for
+        for (unsigned int j = 0; j < 3; ++j) {
             hashTable[i][j].resize(TABLE_SIZE, -1);
+            fill(hashTable[i][j].begin(), hashTable[i][j].end(), -1);
         }
     }
 
     // Check that bucket size is not exceeded while assigning
-    vector<int> bucket_size(noOfBuckets, 0);
-    for (int i = 0; i < N; ++i) {
+    vector<unsigned int> bucket_size(noOfBuckets, 0);
+    for (unsigned int i = 0; i < N; ++i) {
         bucket_size[getBucketNumber(keys[i], noOfBuckets)]++;
     }
     if (*max_element(bucket_size.begin(), bucket_size.end()) > 512) {
@@ -109,17 +116,17 @@ int main() {
     }
     HashFunctions f[3];
     // Numbers to be XOR'ed with random number to get corresponding functions.
-    int XOR_NUM[3][2] = {{69,     696},
+    unsigned int XOR_NUM[3][2] = {{69,     696},
                          {6969,   69696},
                          {696969, 6969696}};
 
     omp_lock_t table_lock[noOfBuckets][3][TABLE_SIZE];
 #pragma omp parallel for
-    for (int i = 0; i < noOfBuckets; ++i) {
+    for (unsigned int i = 0; i < noOfBuckets; ++i) {
 #pragma omp parallel for
-        for (int j = 0; j < 3; ++j) {
+        for (unsigned int j = 0; j < 3; ++j) {
 #pragma omp parallel for
-            for (int k = 0; k < TABLE_SIZE; ++k) {
+            for (unsigned int k = 0; k < TABLE_SIZE; ++k) {
                 omp_init_lock(&table_lock[i][j][k]);
             }
         }
@@ -128,49 +135,73 @@ int main() {
     // Flag which tells if we need to change the hashing functions due to iterations>=25
     bool flag_change_g = false;
     srand(time(NULL));
+    unsigned int iterations = 0;
+    unsigned int left_out = 0;
     do {
         if (flag_change_g) {
+            cout << "Number of keys left out : " << left_out << endl;
             cout << "Last hashing failed, retrying with new random number." << endl;
+#pragma omp parallel for
+            for (unsigned int i = 0; i < noOfBuckets; ++i) {
+#pragma omp parallel for
+                for (unsigned int j = 0; j < 3; ++j) {
+                    fill(hashTable[i][j].begin(), hashTable[i][j].end(), -1);
+                }
+            }
         }
-
         // Set up hashing functions
-        int rand_number = rand();   //Works well for 1804289383
-        for (int i = 0; i < 3; ++i) {
+        unsigned int rand_number = rand();
+        for (unsigned int i = 0; i < 3; ++i) {
             f[i] = HashFunctions(XOR_NUM[i][0] ^ rand_number, XOR_NUM[i][1] ^ rand_number);
         }
 
         cout << "Random number : " << rand_number << endl;
+
+        // Set up flags.
         flag_change_g = false;
+        left_out = 0;
+        iterations = 0;
 
+        // Cuckoo Hash in action.
+        // Keep running until number is saved in the table or infinite loop (iterations>25) is reached
+        while (iterations < 25) {
 #pragma omp parallel for
-        for (int i = 0; i < N; ++i) {
-            int bucketNumber = getBucketNumber(keys[i], noOfBuckets);
-            int g[3];
+            for (unsigned int i = 0; i < N; ++i) {
+                unsigned int bucketNumber = getBucketNumber(keys[i], noOfBuckets);
+                unsigned int tableNumber = iterations % 3;
+                unsigned int g[3];
 
-            // Get the index in each Hash Table.
-            for (int j = 0; j < 3; ++j) {
-                g[j] = f[j].g(keys[i]);
-            }
-
-            int iterations = 0;
-
-            // Cuckoo Hash in action.
-            // Keep running until number is
-            while (iterations < 25) {
-                for (int j = 0; j < 3; ++j) {
-                    if (hashTable[bucketNumber][0][g[0]] != keys[i] && hashTable[bucketNumber][1][g[1]] != keys[i] &&
-                        hashTable[bucketNumber][2][g[2]] != keys[i]) {
-                        omp_set_lock(&table_lock[bucketNumber][j][g[j]]);
-                        hashTable[bucketNumber][j][g[j]] = keys[i];
-                        omp_unset_lock(&table_lock[bucketNumber][j][g[j]]);
-                    }
-#pragma omp barrier
-                    iterations++;
+                // Get the index in each Hash Table.
+                for (unsigned int j = 0; j < 3; ++j) {
+                    g[j] = f[j].g(keys[i]);
+                }
+                if (hashTable[bucketNumber][0][g[0]] != keys[i] && hashTable[bucketNumber][1][g[1]] != keys[i] &&
+                    hashTable[bucketNumber][2][g[2]] != keys[i]) {
+                    omp_set_lock(&table_lock[bucketNumber][tableNumber][g[tableNumber]]);
+                    hashTable[bucketNumber][tableNumber][g[tableNumber]] = keys[i];
+                    omp_unset_lock(&table_lock[bucketNumber][tableNumber][g[tableNumber]]);
                 }
             }
-            if (hashTable[bucketNumber][0][g[0]] != keys[i] && hashTable[bucketNumber][1][g[1]] != keys[i] && hashTable[bucketNumber][2][g[2]] != keys[i]) {
+            iterations++;
+        }
+        // This part checks whether any of the keys are left out, if yes then regenerate the hashing functions.
+#pragma omp parallel for
+        for (unsigned int i = 0; i < N; ++i) {
+            unsigned int bucketNumber = getBucketNumber(keys[i], noOfBuckets);
+            unsigned int g[3];
+
+            // Get the index in each Hash Table.
+            for (unsigned int j = 0; j < 3; ++j) {
+                g[j] = f[j].g(keys[i]);
+            }
+            if (hashTable[bucketNumber][0][g[0]] != keys[i] && hashTable[bucketNumber][1][g[1]] != keys[i] &&
+                hashTable[bucketNumber][2][g[2]] != keys[i]) {
 #pragma omp critical
-                flag_change_g = true;
+                {
+                    // Count of left out keys
+                    left_out++;
+                    flag_change_g = true;
+                }
             }
         }
     } while (flag_change_g);
